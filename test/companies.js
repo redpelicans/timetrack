63908006c538ccf640ede5223d8062530e989484
Eric <eric.basley@redpelicans.com>
2015-09-04 11:24:35 +0000

    mocha + travis

---
+++ test/companies.js
@@ -0,0 +1,36 @@
+//assertion library
+import  should from "should";
+import  async from "async";
+import  _ from "lodash";
+import {Client} from '../server/lib/models';
+// help to connect, load data to MongoDB
+import * as DB from './helpers/db';
+// sample data to load
+import {data} from './data/clients';
+
+describe('Client', () => {
+  // call once foreach  describe()
+  before(cb => DB.connect('tests', cb))
+
+  // call foreach  it()
+  beforeEach(done => {
+    DB.drop( err => {
+      if(err) done(err);
+      DB.load(data, done);
+    })
+  })
+
+  // call once after each  describe()
+  after( done => DB.close(done) )
+
+  // it() here is async
+  it('Check clients data loading', (cb) => {
+    Client.findAll({type: 'client'}, (err, clients) => {
+      if(err) return cb(err);
+      let count = _.filter(data['collections']['companies'], client => client.type === 'client').length;
+      should(clients.length).equal(count);
+      cb();
+    });
+  });
+
+});
---
+++ test/data/clients.js
@@ -0,0 +1,61 @@
+export let data={
+  collections:{
+    clients: [
+      {
+        "_id" : 1,
+        "type" : "client",
+        "name" : "Jacobs - Waters",
+        "phones" : [
+          {
+            "label" : "sensor",
+            "phone" : "(282) 360-9506 x474"
+          },
+          {
+            "label" : "card",
+            "phone" : "(912) 039-8735"
+          },
+          {
+            "label" : "bus",
+            "phone" : "1-939-925-5062 x9918"
+          }
+        ],
+        "address" : {
+          "number" : 52809,
+          "street" : "Spencer Lights",
+          "country" : "San Marino"
+        },
+        "website" : "http://rahul.info",
+        "color" : "#7d703a",
+        "avatar" : "https://s3.amazonaws.com/uifaces/faces/twitter/sdidonato/128.jpg",
+        "legalForm" : "back-end",
+        "createdAt" : new Date(),
+        "note" : "Incidunt at quisquam libero sed enim.\nCommodi aut enim nulla aspernatur.\nImpedit perspiciatis repellendus qui sint.\nVelit inventore ipsam quis.\nVoluptatem fuga dolore.\n \rEum excepturi sint eligendi porro rerum amet dolores rem.\nUt tempore itaque vitae maiores ea molestias assumenda vero quam.\nProvident aperiam incidunt tempora.\nQuibusdam qui sequi omnis error laborum illum rerum accusantium.\nVel quibusdam occaecati aut in ut ipsa est.\n \rSit vel placeat quia minima consectetur.\nNostrum corporis eveniet ipsa rerum.\nCumque culpa voluptate facere et hic vel dolorem cupiditate officiis.\nQui voluptatem dolor ut molestiae eum dicta aut.\nEst in explicabo et pariatur architecto et vel suscipit sed.\nConsectetur est sit vel inventore incidunt iste et vel molestiae." 
+      },
+      {
+        "_id" : 2,
+        "type" : "client",
+        "name" : "Kub Inc",
+        "phones" : [
+          {
+            "label" : "capacitor",
+            "phone" : "1-011-622-2223 x704"
+          },
+          {
+            "label" : "bandwidth",
+            "phone" : "666.354.6605 x02463"
+          }
+        ],
+        "address" : {
+          "number" : 89247,
+          "street" : "Crist Drive",
+          "country" : "Comoros"
+        },
+        "website" : "http://oma.net",
+        "color" : "#3b0413",
+        "avatar" : "https://s3.amazonaws.com/uifaces/faces/twitter/chaabane_wail/128.jpg",
+        "legalForm" : "wireless",
+        "createdAt" : new Date(),
+        "note" : "Enim aut quia distinctio.\nAut voluptas ut dolorum.\nSit et qui quo repellendus corrupti ad omnis maiores qui.\nCulpa id nam totam.\nEst ad dolor nam.\nNemo et qui culpa nesciunt non.\n \rTempora doloribus itaque perspiciatis enim illum vel excepturi aut incidunt.\nQuisquam perferendis deleniti.\nSed et blanditiis nam et quo.\n \rTenetur officiis voluptas eaque nisi ut aut sit voluptatem.\nHarum dolor molestiae.\nCupiditate voluptatibus et est aliquid nisi repudiandae.\nNulla ipsa qui consequatur aperiam.\nQuia voluptatum est eligendi odio omnis aliquam." 
+      }]
+    }
+}
