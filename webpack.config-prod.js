36c335ea0fd7d74951340e2b2778625bc9b99af4
Eric <eric.basley@redpelicans.com>
2015-12-11 11:01:39 +0000

    fix webpack for production

---
+++ webpack.config-prod.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+var params = require('./params.js');
+var port = params.proxy.port || 6806;
+
+module.exports = {
+  devtool: '#eval-source-map',
+  entry: [
+    path.join(__dirname, 'app', 'index')
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ 'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch' }),
+    //new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    preLoaders: [
+      {test: /\.jsx?$/, loaders: ['regenerator'], include: path.join(__dirname, 'app')}
+    ],
+    loaders: [
+      {test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/, include: path.join(__dirname, 'app'), stage: 0},
+      {test: /\.css$/, loader: 'style!css!autoprefixer-loader?browsers=last 2 versions'},
+      {test: /\.less$/, loader: 'style!css!autoprefixer-loader?browsers=last 2 versions!less'},
+      {test: /\.gif$/, loader: "url-loader?mimetype=image/png"},
+      { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
+    ]
+  },
+}
