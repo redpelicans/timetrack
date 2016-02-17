1da364610a1611253e6fe7ea3f3544a5214092de
ebasley <eric.basley@redpelicans.com>
2016-02-17 14:23:51 +0100

    merge with isomorphic

--- dist/client/containers/root.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./root.prod');
-} else {
-  module.exports = require('./root.dev');
-}
-//# sourceMappingURL=root.js.map
--- dist/client/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.log('Running in ' + process.env.NODE_ENV + ' mode');
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
-}
-//# sourceMappingURL=configureStore.js.map
