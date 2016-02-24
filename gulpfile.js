8f0730cc9bc125db325cbe06ee262e9121b7f72b
Nicolas Briemant <nicolas.briemant@gmail.com>
2016-02-24 17:19:29 +0100

    change way gulp is launching node processes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var del = require('del');
 var vinylPaths = require('vinyl-paths');
 var babel = require('gulp-babel');
+var Cache = require('gulp-file-cache')
 var changed = require('gulp-changed');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
@@ -9,6 +10,7 @@ var nodemon = require('gulp-nodemon');
 var runSequence = require('run-sequence');
 var merge = require('merge-stream');
 var path = require('path');
+var spawn = require('child_process').spawn;
 
 // need this options to use decorators
 var compilerOptions = {
@@ -23,14 +25,18 @@ var compilerOptions = {
 //  presets: ['react', 'es2015'],
 };
 
+var cache = new Cache();
+
 gulp.task('transpile', function () {
   // tanspile from src to dist
   var build =  gulp.src('./src/**')
+    //.pipe(cache.filter()) // remember files
     .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(changed('./dist/', {extension: '.js'}))
     .pipe(babel(compilerOptions))
     .pipe(sourcemaps.write('.', { sourceRoot: './src'}))
+    //.pipe(cache.cache()) // cache them
     .pipe(gulp.dest('./dist/'));
 
   // package.json is read by main.js, so we need it at root level: /server/dist
@@ -49,36 +55,43 @@ gulp.task('clean-dist', function() {
     .pipe(vinylPaths(del));
 });
 
-gulp.task('run-server', function (cb) {
-  var started = false;
-  nodemon({
-      script: './proxy.js'
-    , verbose: true
-    , ext: 'js ejs json'
-    //, watch: [ './dist/server/', './views/']
-    , ignore: ['*.swp',  "*.js.map", './src/client/' ]
-    , env: { DEBUG: 'timetrack:*' }
-  })
-  //.on('restart', function(files){ console.log("node restarted")})
-  .on('start', function() { 
-    console.log('node started') 
-    if(!started){
-      started = true;
-      cb();
-    }
-  })
-});
-
-gulp.task('watch-server', ['transpile'], function() { 
-  gulp.watch('./src/server/**', ['transpile']).on('change', reportChange);
-});
-
 gulp.task('watch-client', function() { 
   gulp.watch('./src/client/**').on('change', reportChange);
 });
 
-gulp.task('run', ['watch-server', 'watch-client'], function(cb){
-  return runSequence( 'run-server', cb )
+gulp.task('run-proxy', ['watch-server', 'watch-client'], function() {
+   function runit(){
+    var env = Object.create( process.env );
+    env.DEBUG = 'timetrack:*';
+    return spawn(
+      'node', 
+      ['./proxy.js'], 
+      { 
+        stdio: 'inherit', 
+        env: env
+      }
+    )
+   }
+   var proc = runit();
+   var watcher = gulp.watch(['./proxy.js', 'webpack.config.js']);
+   watcher.on('change', function(event){
+      reportChange(event);
+      proc.kill();
+      proc = runit();
+   });
 });
+
+gulp.task('watch-server', ['transpile'], function () {
+  var stream = nodemon({
+                 script: './dist/server/main.js' 
+               , watch: './src/server' 
+               , tasks: ['transpile'] 
+               , env: { 'DEBUG': 'timetrack:*' }
+               })
+  return stream
+})
+
+
+gulp.task('run', ['watch-server', 'run-proxy']);
 
 gulp.task('default', ['run']);
--- proxy.js
+++ proxy.js
@@ -7,62 +7,56 @@ var logerror = require('debug')('timetrack:error');
 var params = require('./params.js');
 require('better-log').install();
 
-var server = require('./dist/server/index');
-server.create(params)
-  .then( () =>{ 
-    loginfo('ready to track time with U ...');
-    var proxy = httpProxy.createProxyServer({
-      changeOrigin: true, 
-      ws: true,
-      //target: params.webpack.url
-    }); 
+var proxy = httpProxy.createProxyServer({
+  changeOrigin: true, 
+  ws: true,
+  //target: params.webpack.url
+}); 
 
-    var app = express();
+var app = express();
 
-    // switch to server
-    //app.use(express.static(path.resolve(__dirname, 'public')));
+// switch to server
+//app.use(express.static(path.resolve(__dirname, 'public')));
 
-    var bundle = require('./bundle.js');
-    bundle();
+var bundle = require('./bundle.js');
+bundle();
 
-    app.all('/build/*', function (req, res) {
-      proxy.web(req, res, {target: params.webpack.url});
-    });
+app.all('/build/*', function (req, res) {
+  proxy.web(req, res, {target: params.webpack.url});
+});
 
-    // app.all('/socket.io*', function (req, res) {
-    //   proxy.web(req, res, {target: params.webpack.url});
-    // });
+// app.all('/socket.io*', function (req, res) {
+//   proxy.web(req, res, {target: params.webpack.url});
+// });
 
-    app.all('/sockjs-node/*', function (req, res) {
-      proxy.web(req, res, {target: params.webpack.url});
-    });
+app.all('/sockjs-node/*', function (req, res) {
+  proxy.web(req, res, {target: params.webpack.url});
+});
 
 
-    app.all('*', function (req, res) {
-      proxy.web(req, res, {target: params.server.url});
-    });
+app.all('*', function (req, res) {
+  proxy.web(req, res, {target: params.server.url});
+});
 
-    proxy.on('error', function(err) {
-      logerror("Proxy Error:");
-      console.error(err);
-    });
+proxy.on('error', function(err) {
+  logerror("Proxy Error:");
+  console.error(err);
+});
 
-    // http service to proxy websocket requests from webpack
-    var server = http.createServer(app);
+// http service to proxy websocket requests from webpack
+var server = http.createServer(app);
 
 
-    server.on('upgrade', function (req, socket, head) {
-      //console.log(req.url)
-      if(req.url.match(/^\/sockjs/)){
-        proxy.ws(req, socket, head, {target: params.webpack.url});
-      }else if(req.url.match(/^\/socket.io/)){
-        proxy.ws(req, socket, head, {target: params.server.url});
-      }
-    });
+server.on('upgrade', function (req, socket, head) {
+  //console.log(req.url)
+  if(req.url.match(/^\/sockjs/)){
+    proxy.ws(req, socket, head, {target: params.webpack.url});
+  }else if(req.url.match(/^\/socket.io/)){
+    proxy.ws(req, socket, head, {target: params.server.url});
+  }
+});
 
-    console.info('starting proxy...');
-    server.listen(params.proxy.port, params.proxy.host || '0.0.0.0', function (err) {
-      if(err)return logerror(err);
-      loginfo('proxy listening on: ' + params.proxy.url);
-    }); 
-  }).catch(err => { throw err });
+server.listen(params.proxy.port, params.proxy.host || '0.0.0.0', function (err) {
+  if(err)return logerror(err);
+  loginfo('proxy listening on: ' + params.proxy.url);
+}); 
