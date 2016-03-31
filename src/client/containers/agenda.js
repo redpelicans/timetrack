d7599f081a99ea93da9db2d2ad7a98d4720f985b
Jeremy Vincent <jeremy.vincent@redpelicans.com>
2016-03-31 15:16:02 +0200

    calendarkit, radium test

--- src/client/containers/agenda.js
+++ src/client/containers/agenda.js
@@ -1,84 +1,155 @@
-import React,{Component, PropTypes} from 'react'
-import _ from 'lodash'
+import React,{Component} from 'react'
+import Radium from 'radium'
 
-import moment from 'moment'
-moment.locale('fr')
-
-function numberOfDays({month, year}) {
-  return moment({month, year}).endOf('month').format('D')
-}
-
-const date = {
-  year:  2016,
-  month: 1,
-  day:   18,
-}
-
+import CalendarBase,{CalendarGrid, WeekDays} from './calendarkit'
 ////////////////////////////////////////////////////////////////////////////////
-// AgendaCell
-const AgendaCell = ({
-  date,
-  id,
-}) => {
+// Cells
 
-  const styles = {
-    display: "flex",
-    flexBasis: "14.2857%",
-    minHeight: "100px",
-    backgroundColor: "#434857",
-    border: "1px solid dodgerblue",
+// Awesome Clicky cell
+class ClickyCell extends Component {
+
+  state = {click: false}
+
+  header = ({id, date}) =>
+    <div>{id}</div>
+
+  content = ({id, date}) =>
+    <div>{date.date()}</div>
+
+  handleClick = ({id, date}) => (e) => {
+    this.setState({click: !this.state.click})
   }
 
+  render () {
+    const { id, date } = this.props
+    return (
+      <div
+        style={{
+          height: "100%",
+          backgroundColor: "#434857",
+          border: this.state.click ? "1px solid firebrick": "1px solid dodgerblue",
+        }}
+        onClick={this.handleClick(this.props)}
+      >
+        {this.header(this.props)}
+        {this.content(this.props)}
+      </div>
+    )
+  }
+}
+
+// Mini agenda cell
+const MiniCell = ({id, date}) =>
+  <div onClick={(e) => { alert(date.date()) }}>
+    {date.date()}
+  </div>
+
+// POC Pokemon - pokepoc ;)
+const PokeCell = ({id, date}) => {
+  const styles = { width: "100%" }
   return (
-    <div style={styles}>{id}</div>
+    <img style={styles} src={`http://pokeapi.co/media/sprites/pokemon/${date.date()}.png`} />
   )
 }
-// -------------------------------------------------------------------------- //
-AgendaCell.propTypes = {
-  id: PropTypes.number.isRequired,
-}
-////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-// AgendaGrid
-const AgendaGrid = ({
-  date,
-}) => {
+// Agenda Stateless Functionnal Components <3
+const _Month = ({date, style}) =>
+  <span style={[style]}>{date.format('MMMM')}</span>
+const Month = Radium(_Month)
 
-  const styles = {
-    display: "flex",
-    flexWrap: "wrap",
-    paddingTop: "15px",
-  }
+const _Year = ({date, style}) =>
+  <span style={style}>{date.format('YYYY')}</span>
+const Year = Radium(_Year)
 
-  function generateCells (date) {
-    return _.times(35, (id) => {
-      return <AgendaCell date={date} id={id} />
-    })
-  }
+const _PrevMonth = (props) =>
+  <button {...props}>{'<<<'}</button>
+const PrevMonth = Radium(_PrevMonth)
 
-  return (
-    <div style={styles}>
-      {generateCells(date)}
-    </div>
-  )
-}
-// -------------------------------------------------------------------------- //
-AgendaGrid.propTypes = {
-  date: PropTypes.object.isRequired,
-}
-////////////////////////////////////////////////////////////////////////////////
+const _NextMonth = (props) =>
+  <button {...props}>{'>>>'}</button>
+const NextMonth = Radium(_NextMonth)
+
+const _Today = (props) =>
+  <button {...props}>{'today'}</button>
+const Today = Radium(_Today)
 
 ////////////////////////////////////////////////////////////////////////////////
 // AgendaApp
-const AgendaApp = () => {
-  return (
-    <AgendaGrid date={date} />
-  )
-}
-// -------------------------------------------------------------------------- //
-AgendaApp.propTypes = {
+class AgendaApp extends CalendarBase {
+
+  render () {
+    // public
+    // - state { today, date } // State
+    // - Calendar              // Component
+    // - composeCell           // function
+    // - prevMonth             // function
+    // - nextMonth             // function
+    // - today                 // function
+
+    const styles = {
+      Cell: {
+        height: "20%",
+      },
+      Header: {
+        height: "70px",
+      },
+      CalendarGrid: {
+        height: "calc(100% - 70px)",
+        '@media (max-width: 480px)': {
+          flexDirection: "column"
+        }
+      },
+      _Month: {
+        display: "inline-block",
+        textAlign: "center",
+        width: "100px"
+      },
+      _Year: {
+        display: "inline-block",
+        width: "50px"
+      },
+      _WeekDays: {
+        week: {
+          '@media (max-width: 480px)': {
+            display: "none"
+          }
+        },
+        day: {
+        }
+      }
+    }
+
+    const Calendar      = this.Calendar
+    const Header        = this.container('Header')
+    const Container     = this.container('Container')
+    const CellComponent = this.composeCell(ClickyCell, styles.Cell)
+
+    return (
+      <Calendar styles={styles}>
+        <Header>
+          <Today onClick={this.reset} />
+          <PrevMonth onClick={this.prevMonth} />
+          <Month />
+          <Year />
+          <NextMonth onClick={this.nextMonth} />
+          <WeekDays />
+        </Header>
+        <CalendarGrid CellComponent={CellComponent} />
+      </Calendar>
+    )
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////
-
-export default AgendaApp
+const Wrapper = () =>
+  <div style={{margin: "0 auto", width: "100%"}}>
+    <div style={{float: "left", width: "150px", height: "calc(100vh - 55px)", backgroundColor: "black"}}>
+      content
+    </div>
+    <div style={{float: "left", width: "calc(100% - 150px)", height: "calc(100vh - 55px)"}}>
+      <AgendaApp />
+    </div>
+  </div>
+////////////////////////////////////////////////////////////////////////////////
+export default Wrapper
+////////////////////////////////////////////////////////////////////////////////
